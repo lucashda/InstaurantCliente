48e76fbe579103bfeaede67c010ee4993290c865
lucashda <48419249+lucashda@users.noreply.github.com>
2019-04-20 21:53:34 -0300

    Update LoginActivity.java

--- app/src/main/java/com/ondeline/InstaurantCliente/LoginActivity.java
+++ app/src/main/java/com/ondeline/InstaurantCliente/LoginActivity.java
@@ -19,7 +19,6 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LoginActivity extends AppCompatActivity {
 
     EditText email, senha;
-    TextView cadastrar;
     Button btnLogin;
 
     FirebaseAuth firebaseAuth;
